bae9eb2b11c630a95022411af6bb0b56d434e7ba
Jordan Conard <jconard@rsglab.com>
2017-12-05 16:36:00 -0500

    Solution to day 5 of Advent of Code.

---
+++ day-5/solution.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	// Read input, convert to ints and place into array
+	f, err := os.Open("input.txt")
+	//f, err := os.Open("test.txt")
+	if err != nil {
+		fmt.Println("Could not open input.txt. Exiting")
+		os.Exit(-1)
+	}
+	defer f.Close()
+
+	var list []int
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		i, _ := strconv.Atoi(s.Text())
+		list = append(list, i)
+	}
+
+	//Start processing
+	pos := 0   //Initialize the current position
+	count := 0 //initialize the count
+	length := len(list)
+	for !(pos >= length) {
+		count = count + 1
+		jump := list[pos]
+		list[pos] = list[pos] + 1
+		pos = pos + jump
+	}
+	fmt.Printf("It took %d steps", count)
+}
